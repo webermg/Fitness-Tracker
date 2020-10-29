2b12010111836017d131dfa54a315cff2b1fb452
webermg <matthew.gerald.weber@gmail.com>
2020-10-29 15:50:31 -0700

    add db to server

--- server.js
+++ server.js
@@ -1,26 +1,20 @@
-// *********************************************************************************
-// This file is the initial starting point for the Node/Express server.
-// *********************************************************************************
-
-
 // Dependencies
 // =============================================================
 const express = require("express");
 const mongoose = require("mongoose")
-
 const db = require("./models");
-
 
 // Sets up the Express App
 // =============================================================
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//mongodb
+mongoose.connect(process.eng.MONGODB_URI || "mongodb://localhost/fitness_db", {useNewUrlParser: true});
 
 // Static directory to be served
 app.use(express.static("public"));
@@ -40,7 +34,6 @@ app.use("/",require("./controllers/htmlroutes"));
 
 // Starts the server to begin listening
 // =============================================================
-
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
