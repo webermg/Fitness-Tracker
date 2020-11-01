2c7ea4b0e5ccccb72ffa86e91c5cc49ce9b9852b
webermg <matthew.gerald.weber@gmail.com>
2020-10-31 22:42:13 -0700

    added event for create new workout button

---
+++ public/js/home.js
@@ -0,0 +1,15 @@
+document.getElementById("new-button").addEventListener("click", function() {
+  const newWorkout = {};
+  newWorkout.day = Date.now();
+  fetch(`/api/workouts`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(newWorkout)
+  })
+  .then(res => {
+    console.log(res)
+    location.href = "/view"
+  })
+  .catch(err => console.log(err));
+  
+})
