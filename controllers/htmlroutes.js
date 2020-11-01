9270519bcaabe63752c0e505120a2ac48d951326
webermg <matthew.gerald.weber@gmail.com>
2020-10-31 17:59:01 -0700

    added html routes

---
+++ controllers/htmlroutes.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const db = require("../models")
+
+router.get("/",(req,res) => {
+  db.Workout.find({}).populate("exercises").lean().then(workouts => {
+    res.render("home",{workouts: workouts});
+  })
+})
+
+router.get("/view",(req,res) => {
+  db.Workout.find({})
+  .sort({day:-1})
+  .limit(1)
+  .populate("exercises")
+  .lean()
+  .then(workout => {
+    res.render("view",{workouts: workout});
+  })
+})
+
+module.exports = router;
